5def7e9a83524938e0fe39874343c07856468551
KorolevOl <korolev.oa@weldbook.ru>
2021-04-07 09:52:56 +0300

    regex add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,73 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+    <div>
+      <pre>{{data}}</pre>
+      <hr>
+      <pre [innerHTML]="result"></pre>
+    </div>    
+  `
 })
-export class AppComponent {
-  title = 'regex-angular';
+export class AppComponent implements OnInit{
+  data: string = `
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, impedit! weldbook.ru Aliquam at consectetur culpa dignissimos doloribus eaque libero nostrum quos suscipit unde!
+weldbook.ru/new Aliquam at consectetur culpa dignissimos doloribus eaque libero nostrum quos suscipit unde!
+
+weldbook.ru
+
+http://weldbook.ru
+http://weldbook.ru/new
+https://weldbook.ru/
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. https://weldbook.ru/new Aliquam at consectetur culpa dignissimos doloribus eaque libero nostrum quos suscipit unde!
+
+https://master.review.weldbook.ru/new
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. https://develop.review.weldbook.ru/new Aliquam at consectetur culpa dignissimos doloribus eaque libero nostrum quos suscipit unde!
+
+https://master.review.weldbook.com/new
+https://develop.review.weldbook.com/new
+Lorem https://develop.review.weldbook.com ipsum
+
+weldbook.com
+weldbook.ru/new
+
+https://beta.weldbook.ru/new/laboratory/ndt/journals/tt?qwe=1df
+https://beta.weldbook.ru/new/laboratory/ndt/journals/tt?qwe&asd
+
+Aliquam at consectetur culpa dignissimos doloribus eaque libero nostrum quos suscipit unde!
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. https://beta.weldbook.ru/new/laboratory/ndt/journals/tt?qwe=1&asd=2&zxc=3df Aliquam at consectetur culpa dignissimos doloribus eaque libero nostrum quos suscipit unde!
+`;
+  result: string;
+
+  urlStrObj = {
+    protocol: '(?:(?<protocol>https?)(?::\\/\\/))?',
+    subDomains: '(?<subDomains>(?:[\\w-]+\\.)*)',
+    domainName: '(?<domainName>[\\w-]+)',
+    domainZone: '(?:\\.(?<domainZone>\\w{2,})(?=[\\s\\/]))',
+    urlPath: '(?<urlPath>(?:\\/[\\w-]+)*)',
+    urlPam: '(?:\\?(?<urlPam>\\w+(?:=\\w+)?(?:&\\w+(?:=\\w+)?)*))?'
+  }
+
+  urlStr = Object.values(this.urlStrObj).join('');
+
+  ngOnInit(): void {
+    console.log(this.urlStr)
+    const urlRegex = new RegExp(this.urlStr, 'g')
+
+    const urlMatch = this.data.match(urlRegex);
+
+    console.log(urlMatch);
+
+
+    let resExec
+
+    while (resExec = urlRegex.exec(this.data)) {
+      console.log(resExec);
+    }
+
+    this.result = this.data.replace(urlRegex, '<a href="$&">$&<\a>')
+
+
+  }
 }
